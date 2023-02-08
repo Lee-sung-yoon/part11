096bd3d52f4d772e095afbad4df3f2ba35d548b8
LEE <tjddbs1412@naver.com>
2023-02-08 10:48:32 +0900

    Initial commit

---
+++ src/main/java/com/example/part11/Part11Application.java
@@ -0,0 +1,13 @@
+package com.example.part11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/part11/Part11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.part11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
